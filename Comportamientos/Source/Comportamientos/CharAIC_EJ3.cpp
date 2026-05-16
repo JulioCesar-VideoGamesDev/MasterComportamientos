0118bcf8a15a724a346a175ccce3c7313c5197f9
JulioCesar-VideoGamesDev <1juliocesartrabajo@gmail.com>
2026-05-16 14:55:40 +0200

    Adding gitignore, renaming things from past exercises & adding cpp Classes for Exercise3

---
+++ Comportamientos/Source/Comportamientos/CharAIC_EJ3.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharAIC_EJ3.h"
+
+CharAIC_EJ3::CharAIC_EJ3()
+{
+}
+
+CharAIC_EJ3::~CharAIC_EJ3()
+{
+}
---
+++ Comportamientos/Source/Comportamientos/CharAIC_EJ3.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class COMPORTAMIENTOS_API CharAIC_EJ3
+{
+public:
+	CharAIC_EJ3();
+	~CharAIC_EJ3();
+};
